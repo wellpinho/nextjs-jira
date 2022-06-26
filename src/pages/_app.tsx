2160fcbda30ab45eaa86dc395e9e2c7bc136faf7
wellpinho <wellpinho@outlook.com>
2022-06-26 07:58:42 -0300

    ✨ NEW FEATURE: material ui instalation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
-import "../../styles/globals.scss";
 import type { AppProps } from "next/app";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import "../../styles/globals.scss";
+
+const basicTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={basicTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
